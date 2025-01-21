c9756ec60008625658ead5c2a312c362ed4a23d6
Tom Bourti <sirvos.lol@gmail.com>
2025-01-21 11:45:30 +0100

    feat-6: deposit money + fix-7: check if the amount is positive

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
 from utils.auth import CreateUser, create_user
@@ -15,6 +16,11 @@ class CreateTransaction(BaseModel):
     source_account_number: str
     destination_account_number: str
     amount: float
+
+class Deposit(BaseModel):
+    account_number: str
+    amount: float
+
 
 accounts = [
     {
@@ -36,20 +42,45 @@ accounts = [
         "status": "active"
     }
 ]
+history_transaction = [] 
+
 @app.post("/send_money")
 def send_money(body: CreateTransaction):
     source_account = next((account for account in accounts if account["account_number"] == body.source_account_number), None)
     destination_account = next((account for account in accounts if account["account_number"] == body.destination_account_number), None)
 
-    if source_account and destination_account:
-        if source_account["balance"] >= body.amount:
-            source_account["balance"] -= body.amount
-            destination_account["balance"] += body.amount
-            print (accounts)
+    if body.amount > 10:
+        if source_account and destination_account:
+            if source_account["balance"] >= body.amount:
+                source_account["balance"] -= body.amount
+                destination_account["balance"] += body.amount
+                print (accounts)
+                return {"status": "success"}
+            else:
+                return {"status": "error", "message": "Insufficient funds"}
+        else:
+            return {"status": "error", "message": "Accounts not found"}
+    else : 
+        return {"status": "error", "message": "Amount must be greater than 10"}
+
+
+@app.post("/deposit")
+def deposit(body: Deposit):
+    account = next((account for account in accounts if account["account_number"] == body.account_number), None)
+    if body.amount > 10 :
+        if account:
+            account["balance"] += body.amount
+            history_transaction.append(
+                {
+                    "action": "deposit",
+                    "amount": body.amount,
+                    "timestamp": datetime.now()
+                }
+            )
+            print(history_transaction)
+            print(accounts) 
             return {"status": "success"}
         else:
-            return {"status": "error", "message": "Insufficient funds"}
-    else:
-        return {"status": "error", "message": "Accounts not found"}
-
-
+            return {"status": "error", "message": "Account not found"}
+    else :
+        return {"status": "error", "message": "Amount must be greater than 10"}
