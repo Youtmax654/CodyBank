14cade6c1e64f157718b4b4c0608744ea242a061
Youtmax654 <pennmaxime@gmail.com>
2025-01-21 14:15:07 +0100

    refactor the code

--- main.py
+++ main.py
@@ -1,106 +1,24 @@
-from datetime import datetime
 from fastapi import FastAPI
-from pydantic import BaseModel
 from utils.auth import CreateUser, create_user
 from utils.db import create_db_and_tables
+from utils.transactions import SendMoney, send_money, Deposit, deposit, get_transactions
 
 create_db_and_tables()
 
 app = FastAPI()
 
 @app.post("/register")
-def create_user(body: CreateUser):
+def handler(body: CreateUser):
     create_user(body)
 
-class CreateTransaction(BaseModel):
-    source_account_number: str
-    destination_account_number: str
-    amount: float
-
-class Deposit(BaseModel):
-    account_number: str
-    amount: float
-
-
-accounts = [
-    {
-        "id": 1,
-        "user_id": 1,
-        "account_number": "123456789",
-        "balance": 1000.00,
-        "is_primary": True,
-        "created_at": "2024-12-14T10:00:00",
-        "status": "active"
-    },
-        {
-        "id": 2,
-        "user_id": 2,
-        "account_number": "987654321",
-        "balance": 500.00,
-        "is_primary": True,
-        "created_at": "2024-12-14T11:00:00",
-        "status": "active"
-    }
-]
-transactions = [] 
-
 @app.post("/send_money")
-def send_money(body: CreateTransaction):
-    source_account = next((account for account in accounts if account["account_number"] == body.source_account_number), None)
-    destination_account = next((account for account in accounts if account["account_number"] == body.destination_account_number), None)
-
-    if body.amount > 10:
-        if source_account and destination_account:
-            if source_account["balance"] >= body.amount:
-                source_account["balance"] -= body.amount
-                destination_account["balance"] += body.amount
-                transactions.append(
-                    {
-                        "amount": body.amount,
-                        "source_account_number":body.source_account_number,
-                        "destination_account_number":body.destination_account_number,
-                        "type":"send",
-                        "timestamp": datetime.now()
-                    }
-                )
-                print(transactions)
-                print (accounts)
-                return {"status": "success"}
-            else:
-                return {"status": "error", "message": "Insufficient funds"}
-        else:
-            return {"status": "error", "message": "Accounts not found"}
-    else : 
-        return {"status": "error", "message": "Amount must be greater than 10"}
-
+def handler(body: SendMoney):
+    send_money(body)
 
 @app.post("/deposit")
-def deposit(body: Deposit):
-    account = next((account for account in accounts if account["account_number"] == body.account_number), None)
-    if body.amount > 10 :
-        if account:
-            account["balance"] += body.amount
-            transactions.append(
-                {
-                    "amount": body.amount,
-                    "destination_account_number":body.account_number,
-                    "type":"deposit",
-                    "timestamp": datetime.now()
-                }
-            )
-            print(transactions)
-            print(accounts) 
-            return {"status": "success"}
-        else:
-            return {"status": "error", "message": "Account not found"}
-    else :
-        return {"status": "error", "message": "Amount must be greater than 10"}
-    
+def handler(body: Deposit):
+    deposit(body)
+
 @app.get("/transactions")
-def transactions(account_number : str):
-    account_transactions = []
-    for transaction in transactions:
-        if transaction("destination_account_number") == account_number or transaction("source_account_number") == account_number:
-            account_transactions.append(transaction)
-    account_transactions.sort(key=lambda x: x["timestamp"], reverse=True)
-    return {"status": "success"}
+def handler(account_number : str):
+  return get_transactions(account_number)
