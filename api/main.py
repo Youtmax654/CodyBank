77da664d1082ee8d536ea3b8f438d2e9009a3404
Youtmax654 <pennmaxime@gmail.com>
2025-01-30 10:12:36 +0100

    chore: regenerate routeTree and remove unused cors config

--- api/main.py
+++ api/main.py
@@ -15,18 +15,6 @@ create_db_and_tables()
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.on_event("startup")
 @repeat_every(seconds=10)
