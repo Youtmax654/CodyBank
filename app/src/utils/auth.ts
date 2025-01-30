422e81ed22c737d29349babbd12908a3946e098f
Enzo <enzopohu91@gmail.com>
2025-01-30 16:22:17 +0100

    feat: function delete account

--- app/src/utils/auth.ts
+++ app/src/utils/auth.ts
@@ -33,3 +33,25 @@ export function isAuthenticated() {
   const decoded = jwtDecode<UserToken>(token);
   return !!decoded.user_id;
 }
+
+export async function checkPassword(body: { password: string }) {
+  const token = Cookies.get("token");
+  if (!token) return false;
+
+  const userRes = await axios.get("/api/me", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const user = userRes.data;
+
+  try {
+    const res = await axios.post(`/api/login`, {
+      email: user.email,
+      password: body.password,
+    });
+    return res.status === 200;
+  } catch (error) {
+    return false;
+  }
+}
